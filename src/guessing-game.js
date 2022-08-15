cf17d78b6babf2d1ae9d2f098f61fb1e67fb40ea
Anastasia Sytsevich <drozdushka@gmail.com>
2022-08-15 20:45:30 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,18 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minV = min;
+        this.maxV = max;
     }
 
     guess() {
-
+        return Math.round((this.minV+this.maxV)/2);
     }
-
     lower() {
-
+        this.maxV = Math.round((this.minV+this.maxV)/2);
     }
-
     greater() {
-
+        this.minV = Math.round((this.minV+this.maxV)/2);
     }
 }
 
